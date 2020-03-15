63bb749c8003c63dd88fe9ed1eeb6fbb91dd100e
serrrgun <serrrgun@mail.ru>
2020-03-15 09:49:28 +0300

    feat: add header script & add portfolio script & delete files

---
+++ script.js
@@ -0,0 +1,74 @@
+// header
+
+const arrLinks = document.querySelectorAll('.navigation__item');
+const arrBlocks = document.querySelectorAll('.js-link-block');
+const header = document.querySelector('.header');
+
+function anim(duration) {
+    let temp;
+    return function(sel) {
+        cancelAnimationFrame(temp);
+        const start = performance.now();
+        const from = window.pageYOffset || document.documentElement.scrollTop,
+        to = sel.getBoundingClientRect().top - header.offsetHeight;
+        requestAnimationFrame(function step(timestamp) {
+            var progress = (timestamp - start) / duration;
+            1 <= progress && (progress = 1);
+            window.scrollTo(0, from + to * progress | 0);
+            1 > progress && (temp = requestAnimationFrame(step))
+        })
+    }
+};
+
+const scrollMenu = anim(500);
+
+arrLinks.forEach((elem, index) => {
+    elem.addEventListener('click', function() {
+        
+        scrollMenu(arrBlocks[index])
+        
+        arrLinks.forEach(elem => {
+            elem.classList.remove('navigation__item--active')
+        })
+        elem.classList.add('navigation__item--active')
+    })
+})
+
+// --header
+
+// portfolio
+
+const filtersButtons = document.querySelectorAll('.filter__btn');
+const workList = [...document.querySelectorAll('.work-list__item')];
+const workListContainer = document.querySelector('.work-list')
+
+filtersButtons.forEach(btn => {
+    btn.addEventListener('click', function() {
+        filtersButtons.forEach(button => {
+            button.classList.remove('filter__btn--active')
+        })
+        btn.classList.add('filter__btn--active')
+        workList.forEach(work => {
+            work.remove()
+        });
+        workList.sort(function(){
+            return Math.random() - 0.5;
+        });
+        workList.forEach(work => {
+            workListContainer.append(work)
+            work.style.outline = 'none'
+        });
+    })
+})
+
+workList.forEach(elem => {
+    elem.addEventListener('click', function(evt) {
+        evt.preventDefault()
+        workList.forEach(item => {
+            item.style.outline = 'none'
+        })
+        elem.style.outline = '5px solid #F06C64'
+    })
+})
+
+// -- portfolio
